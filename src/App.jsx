1d0f3f27eb769a8f2de5da7e0ed4f6e30702d334
rindhiya970 <rindhiya731@gmail.com>
2026-04-27 22:41:53 +0530

    Refactor App component structure for improved readability and organization

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,23 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import CardSection from './components/CardSection';
 import Footer from './components/Footer';
+import './App.css';
 
 function App() {
   return (
-    <>
+    <div className="app">
+      {/* Header Component */}
       <Navbar />
-      <Hero />
-      <CardSection />
+      
+      {/* Body Section Component */}
+      <main>
+        <Hero />
+        <CardSection />
+      </main>
+      
+      {/* Footer Component */}
       <Footer />
-    </>
+    </div>
   );
 }
 
