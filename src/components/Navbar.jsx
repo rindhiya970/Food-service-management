97bd261f50bcab11288866f40a1e853a4c80efeb
rindhiya970 <rindhiya731@gmail.com>
2026-04-28 23:14:35 +0530

    Fix typo in Navbar component: change "Foods" to "Food" and adjust spacing in "Get started" button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,13 +25,13 @@ const Navbar = () => {
           <ul>
             <li><a href="#" className="active-link">Home</a></li>
             <li><a href="#">Restaurants</a></li>
-            <li><a href="#">Foods</a></li>
+            <li><a href="#">Food</a></li>
             <li><a href="#">Contact</a></li>
           </ul>
         </div>
         
         <div className="navbar-buttons">
-          <button className="btn-get-started">Get started</button>
+          <button className="btn-get-started">Get  started</button>
           <button className="mobile-menu-btn" onClick={toggleMobileMenu}>
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={isMobileMenuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}></path>
