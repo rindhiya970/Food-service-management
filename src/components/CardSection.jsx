515ea8fcf1a4393be8424094dbc11633ea9e4df1
rindhiya970 <rindhiya731@gmail.com>
2026-04-28 23:14:03 +0530

    Fix name typo in testimonial in CardSection.jsx

--- src/components/CardSection.jsx
+++ src/components/CardSection.jsx
@@ -4,7 +4,7 @@ import '../styles/CardSection.css';
 const testimonials = [
   {
     quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime minima dicta amet, molestiae aliquam incidunt suscipit recusandae labore ratione doloremque, architecto et illo minus quo tenetur ducimus, voluptatibus repellendus fuga aperiam vel ab! Ipsam corrupti blanditiis neque eos, vero quas.",
-    name: "Gladis Lennon",
+    name: "Gladis ",
     role: "Head of SEO",
     image: "https://images.unsplash.com/photo-1603366445787-09714680cbf1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=944&q=80"
   },
