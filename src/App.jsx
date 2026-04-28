e350d01ff71b1cc0ace91862cf1d935617b9fad9
rindhiya970 <rindhiya731@gmail.com>
2026-04-28 23:13:39 +0530

    Fix typo in header component comment in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      {/* Header Component */}
+      {/* Header Compot */}
       <Navbar />
       
       {/* Body Section Component */}
