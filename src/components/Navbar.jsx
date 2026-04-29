bf0b01100ca43744db4ee1625bff5cbfb0779a36
rindhiya970 <rindhiya731@gmail.com>
2026-04-29 23:36:23 +0530

    Fix typos in Navbar component: correct "Resturants" to "Restaurants" and "Get  started" to "Get started"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,14 +24,15 @@ const Navbar = () => {
         <div className={`navbar-links ${isMobileMenuOpen ? 'active' : ''}`}>
           <ul>
             <li><a href="#" className="active-link">Home</a></li>
-            <li><a href="#">Restaurants</a></li>
-            <li><a href="#">Food</a></li>
+            <li><a href="#">Quote</a></li>
+            <li><a href="#">Resturants</a></li>
+            <li><a href="#">Foods</a></li>
             <li><a href="#">Contact</a></li>
           </ul>
         </div>
         
         <div className="navbar-buttons">
-          <button className="btn-get-started">Get  started</button>
+          <button className="btn-get-started">Get started</button>
           <button className="mobile-menu-btn" onClick={toggleMobileMenu}>
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={isMobileMenuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}></path>
