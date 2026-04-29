199121d71e1421377f2f3ae7f300e9c95f7d50a8
rindhiya970 <rindhiya731@gmail.com>
2026-04-29 23:35:58 +0530

    Add ContentSection component with image and text layout

---
+++ src/components/ContentSection.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../styles/ContentSection.css';
+
+const ContentSection = () => {
+  return (
+    <section className="content-section">
+      <div className="container content-container">
+        <div className="content-image">
+          <img 
+            src="https://images.unsplash.com/photo-1583608205776-bfd35f0d9f83?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80" 
+            alt="Beautiful house" 
+          />
+        </div>
+        <div className="content-text">
+          <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, debitis.</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, molestiae! Quidem est esse numquam odio deleniti, 
+            beatae, magni dolores provident quaerat totam eos, aperiam architecto eius quis quibusdam fugiat dicta.
+          </p>
+          <button className="btn-get-in-touch">Get in Touch</button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ContentSection;
