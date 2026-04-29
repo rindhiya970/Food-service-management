0b6cb66f4146216e6a2c6bab3143d88837946131
rindhiya970 <rindhiya731@gmail.com>
2026-04-29 23:45:03 +0530

    Add Home page component with Hero, ContentSection, and CardSection

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import ContentSection from '../components/ContentSection';
+import CardSection from '../components/CardSection';
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <ContentSection />
+      <CardSection />
+    </>
+  );
+};
+
+export default Home;
