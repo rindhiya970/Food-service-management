88c65ea5f587d4df4d3bdbae809f23744873f577
rindhiya970 <rindhiya731@gmail.com>
2026-05-01 23:49:04 +0530

    Add JSDoc comment to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,10 @@ import Hero from '../components/Hero';
 import ContentSection from '../components/ContentSection';
 import CardSection from '../components/CardSection';
 
+/**
+ * Home Page Component
+ * Main landing page with hero, content section, and testimonials
+ */
 const Home = () => {
   return (
     <>
